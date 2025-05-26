e014652f6098c81203b4b3eca99219c5040a8927
VEIPHA <angus@veipha.com>
2025-05-26 18:11:58 +0200

    Update google_sheets.py

--- src/google_sheets.py
+++ src/google_sheets.py
@@ -1,31 +1,29 @@
 import os
+import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 def write_jobs_to_sheet(jobs, sheet_name, tab_name):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
-    # Load secret parts from env vars
-    private_key = os.getenv("GCP_PRIVATE_KEY").replace("\\n", "\n")
-    
     creds_dict = {
         "type": "service_account",
         "project_id": "jobscraper-460818",
-        "private_key_id": "d3651a17fbe96911eea0b644860e70fec9e0ff6b",
-        "private_key": private_key,
+        "private_key_id": "1c7fe7bc6e516fe6a9cc2c78d4bccfc87a686cf6",
+        "private_key": os.environ["GCP_PRIVATE_KEY"],
         "client_email": "jobscraper@jobscraper-460818.iam.gserviceaccount.com",
         "client_id": "103019541166983160511",
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/jobscraper@jobscraper-460818.iam.gserviceaccount.com",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/jobscraper%40jobscraper-460818.iam.gserviceaccount.com",
         "universe_domain": "googleapis.com"
     }
 
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
-
     sheet = client.open(sheet_name).worksheet(tab_name)
+
     header = list(jobs[0].keys()) if jobs else []
     rows = [list(job.values()) for job in jobs]
 
