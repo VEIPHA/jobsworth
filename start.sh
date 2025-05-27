c753777dbee8242ac0c531740695d17cfc45b6a9
VEIPHA <angus@veipha.com>
2025-05-27 12:47:08 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo "[start.sh] Running Python scraper..."
 python3 -m src.scraper
 
 echo "[start.sh] Enriching descriptions..."
-python3 -m src.grabbers.description_grabber
+python3 -m src.description_grabber
